92ec55b8a194364430a9e9408acf1cd411c127ab
XingChangYou <876311197@qq.com>
2020-05-16 23:13:49 +0800

    error写错了随便测试一波

--- surveying-common/src/main/java/com/aaa/qy108/status/DeleteStatus.java
+++ surveying-common/src/main/java/com/aaa/qy108/status/DeleteStatus.java
@@ -10,7 +10,7 @@ public enum  DeleteStatus {
     DELETE_DATA_SUCCESS("22001", "删除数据成功"),
     DELETE_DATA_FAILED("12001", "删除数据失败"),
     DELETE_DATA_NOT_EXIST("12002","数据不存在，删除失败！"),
-    DELETE_DATA_ERROE("12003","出现未知错误，请稍后再试！"),
+    DELETE_DATA_ERROR("12003","出现未知错误，请稍后再试！"),
 
     TEST("11111", "测试一下");
 
